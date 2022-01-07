358e54cde3b6a01f080323b23e92440fb16f5696
Gustavo Roos <gustavohroos@gmail.com>
2022-01-07 00:49:31 -0300

    incomplete and untypped ad page

--- src/pages/PageAd/index.tsx
+++ src/pages/PageAd/index.tsx
@@ -1,24 +1,88 @@
 import { PageContainer } from "../../components/MainComponents";
-import { PageArea } from "./styled";
+import { Fake, PageArea } from "./styled";
 import { useParams } from "react-router-dom";
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import OlxAPI from "../../helpers/OlxAPI";
+
 
 const PageAd = () => {
 
+    const api = OlxAPI();
+    type Props = {
+        id: string
+    }
 
-    const { id } = useParams();
+    const  { id } = useParams();
 
     const [loading, setLoading] = useState(true)
+    const [adInfo, setAdInfo] = useState([])
 
-    alert('ID'+id)
+    useEffect(()=>{
+        const getAdInfo = async (id:string) => {
+            const json = await api.getAd(id, true);
+            setAdInfo(json)
+            setLoading(false)
+        }
+        getAdInfo(id)
+    }, []);
+
+
 
     return (
         <PageContainer>
             <PageArea>
-                Algo
+                
+
+                <div className="leftSide">
+                    <div className="box">
+                        <div className="adImage">
+                            {loading && <Fake height={300}/>}
+                            {!loading &&
+                                <img src={adInfo.images} alt="" />
+                                
+                                }
+                        </div>
+
+                        <div className="adInfo">
+                            <div className="adName">
+                                {loading && <Fake height={20}/>}
+                                {!loading &&
+                                <h1>{adInfo.title}</h1>
+                                
+                                }
+                            </div>
+                            <div className="adDescription">
+                                {loading && <Fake height={100}/>}
+                                {!loading &&
+                                <h1>{adInfo.description}</h1>
+                                
+                                }
+                            </div>
+                        </div>
+                    </div>
+
+                </div>
+
+                <div className="rightSide">
+
+                    <div className="box box--padding">
+                        {loading && <Fake height={20}/>}
+                        {!loading &&
+                                <div>
+                                    nome: {adInfo.userInfo.name}
+                                    email: {adInfo.userInfo.email}
+                                </div>
+                                
+                                }
+                    </div>
+                    <div className="box box--padding">
+                        {loading && <Fake height={40}/>}
+                    </div>
+                </div>
             </PageArea>
         </PageContainer>
     )
+    
 
 }
 
--- src/pages/PageAd/styled.tsx
+++ src/pages/PageAd/styled.tsx
@@ -1,5 +1,49 @@
 import styled from "styled-components";
 
+interface FakeProps {
+    height: number
+}
+
+export const Fake = styled.div<FakeProps>`
+    background-color: #DDD;
+    height: ${props=>props.height || 20}px;
+`;
+
 export const PageArea = styled.div`
-   
+    display: flex;
+    margin-top: 20px;
+
+    .box {
+        background-color: #fff;
+        border-radius: 5px;
+        box-shadow: 0px 0px 4px #999;
+        margin-bottom: 20px;
+    }
+
+    .box--padding {
+        padding: 10px;
+    }
+
+    .leftSide {
+        flex: 1;
+        margin-right: 20px;
+
+        .adImage {
+
+        }
+
+        .adInfo {
+            padding: 10px;
+            .adName {
+                margin: 20px;
+            }
+            .adDescription {
+            
+            }
+        }
+    }
+
+    .rightSide {
+        min-width: 300px;
+    }
 `;
