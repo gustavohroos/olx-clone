5d53aa492e75d97153a4dd72abb4db10ce250e05
Gustavo Roos <gustavohroos@gmail.com>
2022-02-08 17:13:51 -0300

    added requireAuth to private routes

--- src/Routes.tsx
+++ src/Routes.tsx
@@ -1,17 +1,29 @@
 import React from 'react'
-import { Routes, Route, Navigate } from 'react-router-dom'
-import Home from './pages/Home'
-import About from './pages/About'
-import NotFound from './pages/NotFound'
-import SignIn from './pages/SignIn'
-import SignUp from './pages/SignUp'
-import PageAd from './pages/PageAd'
+import { Routes, Route, Navigate } from 'react-router-dom';
+
+import Home from './pages/Home';
+import About from './pages/About';
+import NotFound from './pages/NotFound';
+import SignIn from './pages/SignIn';
+import SignUp from './pages/SignUp';
+import PageAd from './pages/PageAd';
+import AddAd from './pages/AddAd';
+import { RequireAuth } from './helpers/requireAuth';
 
 export default () => {
     return (
         <Routes>
             <Route path='/' element={<Home/>} />
-            <Route path='/sobre' element={<About/>}/>
+            <Route path='/sobre' element={
+                <RequireAuth>
+                    <About/>
+                </RequireAuth>
+            }/>
+            <Route path='/post-an-ad' element={
+                <RequireAuth>
+                    <AddAd/>
+                </RequireAuth>
+            }/>
             <Route path='/signin' element={<SignIn/>}/>
             <Route path='/signup' element={<SignUp/>}/>
             <Route path='/ad/:id' element={<PageAd/>}/>
